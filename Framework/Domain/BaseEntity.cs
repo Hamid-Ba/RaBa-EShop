7eaafe06b12d1487bfc4a58cc73087c8d181334e
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-02-19 23:59:54 +0330

    Add Basics To Framework

---
+++ Framework/Domain/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Domain
+{
+    public class BaseEntity
+    {
+        public long Id { get; private set; }
+        public DateTime CreationDate { get; }
+        public bool IsDelete { get; private set; }
+
+        public BaseEntity()
+        {
+            CreationDate = new DateTime();
+            IsDelete = false;
+        }
+    }
+}
