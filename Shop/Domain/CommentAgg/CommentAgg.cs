ce655ddd828d9d2f58556e173cdf162db337a0ff
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-02-22 09:09:57 +0330

    Add Comment Agg , Comment

---
+++ Shop/Domain/CommentAgg/CommentAgg.cs
@@ -0,0 +1,29 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Domain.CommentAgg.Enums;
+
+namespace Domain.CommentAgg
+{
+    public class CommentAgg : AggregateRoot
+    {
+        public long UserId { get; private set; }
+        public long ProductId { get; private set; }
+        public string Content { get; private set; }
+        public CommentStatus Status { get; private set; }
+
+        public CommentAgg(long userId, long productId, string content, CommentStatus status)
+        {
+            Guard(content);
+
+            UserId = userId;
+            ProductId = productId;
+            Content = content;
+            Status = status;
+        }
+
+        public void ChangeStatus(CommentStatus status) => Status = status;
+
+        public void Guard(string content) => NullOrEmptyDomainDataException.CheckString(content, nameof(content));
+
+    }
+}
---
+++ Shop/Domain/CommentAgg/Enums/CommentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.CommentAgg.Enums
+{
+    public enum CommentStatus
+    {
+        Pending,
+        Accepted,
+        Rejected
+    }
+}
