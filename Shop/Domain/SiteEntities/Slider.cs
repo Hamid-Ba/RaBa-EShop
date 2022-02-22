350614308a5562f35cfc3407af4c2e672b85f0fe
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-02-22 09:12:29 +0330

    Add Site Entities , Slider

---
+++ Shop/Domain/SiteEntities/Slider.cs
@@ -0,0 +1,32 @@
+﻿using Common.Domain;
+using Framework.Domain.ValueObjects;
+
+namespace Domain.SiteEntities
+{
+    public class Slider : BaseEntity
+    {
+        public string Title { get; private set; }
+        public string Link { get; private set; }
+        public string ImageName { get; private set; }
+        public SeoImage SeoImage { get; private set; }
+
+        public Slider(string title, string link, string imageName, SeoImage seoImage)
+        {
+            Title = title;
+            Link = link;
+            ImageName = imageName;
+            SeoImage = seoImage;
+        }
+
+        public void Edit(string title, string link, string imageName, SeoImage seoImage)
+        {
+            Title = title;
+            Link = link;
+
+            if (!string.IsNullOrWhiteSpace(imageName))
+                ImageName = imageName;
+
+            SeoImage = seoImage;
+        }
+    }
+}
