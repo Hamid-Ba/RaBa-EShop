89de0f2a0230a96ee129c7a3fbf6977aa7ed4eb8
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-02-22 09:26:43 +0330

    Add Base Repository Contracts

---
+++ Framework/Domain/Repository/IRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+namespace Framework.Domain.Repository
+{
+    public interface IRepository<TEntity>
+    {
+        //Read ALL
+        Task<IEnumerable<TEntity>> GetAllEntitiesAsync();
+        IEnumerable<TEntity> GetAllEntities();
+
+        //Read ONE
+        Task<TEntity> GetEntityAsyncBy(object id);
+        TEntity GetEntityBy(object id);
+
+        //Create One&Range
+        object AddEntity(TEntity entity);
+        void AddRangeOfEntities(IEnumerable<TEntity> entities);
+        Task<object> AddEntityAsync(TEntity entity);
+        Task AddRangeOfEntitiesAsync(IEnumerable<TEntity> entities);
+
+        //Update One&Range
+        bool UpdateEntity(TEntity entity);
+        void UpdateRangeOfEntities(IEnumerable<TEntity> entities);
+
+        //Delete One&Range
+        bool DeleteEntity(TEntity entity);
+        void DeleteRangeOfEntities(IEnumerable<TEntity> entities);
+
+        bool Exists(Expression<Func<TEntity, bool>> expression);
+        void SaveChanges();
+        Task SaveChangesAsync();
+
+        int GetCountOfEntity();
+        IEnumerable<TEntity> PaginationOfEntity(int currentPage, int pageSize);
+    }
+}
