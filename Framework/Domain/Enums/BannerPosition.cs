68095c978e8ae89be2447cd65137e4e5271f86e8
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-02-22 09:19:22 +0330

    Add Site Entities , Banner

---
+++ Framework/Domain/Enums/BannerPosition.cs
@@ -0,0 +1,19 @@
+﻿namespace Framework.Domain.Enums
+{
+    public enum BannerPosition
+    {
+        Up,
+        Up_Left,
+        Up_Right,
+        Middle,
+        Middle_Left,
+        Middle_Right,
+        Down,
+        Down_Left,
+        Down_Right,
+        Slider_Up,
+        Slider_Left,
+        Slider_Right,
+        Slider_Down
+    }
+}
---
+++ Shop/Domain/SiteEntities/Banner.cs
@@ -0,0 +1,40 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Framework.Domain.Enums;
+using Framework.Domain.ValueObjects;
+
+namespace Domain.SiteEntities
+{
+    public class Banner : BaseEntity
+    {
+        public string Link { get; private set; }
+        public string ImageName { get; private set; }
+        public SeoImage SeoImage { get; private set; }
+        public BannerPosition Position { get; private set; }
+
+        public Banner(string link, string imageName, SeoImage seoImage, BannerPosition position)
+        {
+            Guard(link);
+
+            Link = link;
+            ImageName = imageName;
+            SeoImage = seoImage;
+            Position = position;
+        }
+
+        public void Edit(string link, string imageName, SeoImage seoImage, BannerPosition position)
+        {
+            Guard(link);
+
+            Link = link;
+
+            if (!string.IsNullOrWhiteSpace(imageName))
+                ImageName = imageName;
+
+            SeoImage = seoImage;
+            Position = position;
+        }
+
+        public void Guard(string link) => NullOrEmptyDomainDataException.CheckString(link, nameof(link));
+    }
+}
