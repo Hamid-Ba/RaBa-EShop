16dc94b41f60df63e8a7b357a865b50e050fdd0c
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-02-23 18:50:33 +0330

    Complete Change Status Comment Use Case

---
+++ Shop/Application/CommentAgg/ChangeStatus/ChangeStatusCommentCommand.cs
@@ -0,0 +1,7 @@
+﻿using Domain.CommentAgg.Enums;
+using Framework.Application;
+
+namespace Application.CommentAgg.ChangeStatus
+{
+    public record ChangeStatusCommentCommand(long Id, CommentStatus Status) : IBaseCommand;
+}
---
+++ Shop/Application/CommentAgg/ChangeStatus/ChangeStatusCommentCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.CommentAgg.Repository;
+using Framework.Application;
+
+namespace Application.CommentAgg.ChangeStatus
+{
+    public class ChangeStatusCommentCommandHandler : IBaseCommandHandler<ChangeStatusCommentCommand>
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public ChangeStatusCommentCommandHandler(ICommentRepository commentRepository) => _commentRepository = commentRepository;
+
+        public async Task<OperationResult> Handle(ChangeStatusCommentCommand request, CancellationToken cancellationToken)
+        {
+            var comment = await _commentRepository.GetEntityAsyncBy(request.Id);
+            if (comment is null) return OperationResult.NotFound();
+
+            comment.ChangeStatus(request.Status);
+            await _commentRepository.SaveChangesAsync();
+
+            return OperationResult.Success();
+        }
+    }
+}
