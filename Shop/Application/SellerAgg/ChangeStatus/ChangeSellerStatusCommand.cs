6aa8f45cb5435f036309837da69867ee17ffbd8a
hamid balalzadeh <balalzadehhamid79@gmail.com>
2022-03-01 15:13:39 +0330

    Complete Change Seller Status Use Case

---
+++ Shop/Application/SellerAgg/ChangeStatus/ChangeSellerStatusCommand.cs
@@ -0,0 +1,7 @@
+﻿using Domain.SellerAgg.Enums;
+using Framework.Application;
+
+namespace Application.SellerAgg.ChangeStatus
+{
+    public record ChangeSellerStatusCommand(long SellerId, SellerStatus SellerStatus, string Description) : IBaseCommand;
+}
---
+++ Shop/Application/SellerAgg/ChangeStatus/ChangeSellerStatusCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.SellerAgg.Repository;
+using Framework.Application;
+
+namespace Application.SellerAgg.ChangeStatus
+{
+    internal class ChangeSellerStatusCommandHandler : IBaseCommandHandler<ChangeSellerStatusCommand>
+    {
+        private readonly ISellerRepository _sellerRepository;
+
+        public ChangeSellerStatusCommandHandler(ISellerRepository sellerRepository) => _sellerRepository = sellerRepository;
+
+        public async Task<OperationResult> Handle(ChangeSellerStatusCommand request, CancellationToken cancellationToken)
+        {
+            var seller = await _sellerRepository.GetEntityAsyncBy(request.SellerId);
+            if (seller is null) return OperationResult.NotFound();
+
+            seller.ChangeStatus(request.SellerStatus, request.Description);
+            await _sellerRepository.SaveChangesAsync();
+
+            return OperationResult.Success();
+        }
+    }
+}
