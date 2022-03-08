3fa381424c2ae83253872ff1be0dc196c023356d
hamidba <balalzadehhamid79@gmail.com>
2022-03-08 12:59:11 +0330

    Complete Delete User Address

---
+++ Shop/Application/UserAgg/DeleteAddress/DeleteAddressCommand.cs
@@ -0,0 +1,6 @@
+﻿using Framework.Application;
+
+namespace Application.UserAgg.DeleteAddress
+{
+    public record DeleteAddressCommand(long UserId, long AddressId) : IBaseCommand;
+}
---
+++ Shop/Application/UserAgg/DeleteAddress/DeleteAddressCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.UserAgg.Repository;
+using Framework.Application;
+
+namespace Application.UserAgg.DeleteAddress
+{
+    public class DeleteAddressCommandHandler : IBaseCommandHandler<DeleteAddressCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteAddressCommandHandler(IUserRepository userRepository) => _userRepository = userRepository;
+
+        public async Task<OperationResult> Handle(DeleteAddressCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetEntityAsyncBy(request.UserId);
+            if (user is null) return OperationResult.NotFound();
+
+            user.DeleteAddress(request.AddressId);
+            await _userRepository.SaveChangesAsync();
+
+            return OperationResult.Success();
+        }
+    }
+}
