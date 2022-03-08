d68b1e64f0ed9c8f8972e7a2766d64a76ea603a4
hamidba <balalzadehhamid79@gmail.com>
2022-03-08 12:52:22 +0330

    Complete Edit User Address Use Case

---
+++ Shop/Application/UserAgg/EditAddress/EditAddressCommand.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Framework.Application;
+
+namespace Application.UserAgg.EditAddress
+{
+    public record EditAddressCommand(long UserId,long AddressId, string FullName, string PhoneNumber, string Province, string City, string Address,
+			string PostalCode, string NationalCode) : IBaseCommand;
+}
---
+++ Shop/Application/UserAgg/EditAddress/EditAddressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Domain.UserAgg;
+using Domain.UserAgg.Repository;
+using Framework.Application;
+
+namespace Application.UserAgg.EditAddress
+{
+    public class EditAddressCommandHandler : IBaseCommandHandler<EditAddressCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public EditAddressCommandHandler(IUserRepository userRepository) => _userRepository = userRepository;
+
+        public async Task<OperationResult> Handle(EditAddressCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetEntityAsyncBy(request.UserId);
+            if (user is null) return OperationResult.NotFound();
+
+            var newAddress = new UserAddress(request.FullName, request.PhoneNumber, request.Province, request.City,
+                request.Address, request.PostalCode, request.NationalCode);
+            user.EditAddress(request.AddressId, newAddress);
+
+            await _userRepository.SaveChangesAsync();
+            return OperationResult.Success();
+        }
+    }
+}
+
