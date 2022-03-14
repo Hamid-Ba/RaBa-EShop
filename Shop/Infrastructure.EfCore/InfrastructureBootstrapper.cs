6124ad4d2994aa076ea84b04a90c99c27cfcb804
hamidba <balalzadehhamid79@gmail.com>
2022-03-14 12:11:12 +0330

    - [ ] Create Infrastructure Bootstrapper

---
+++ Shop/Infrastructure.EfCore/InfrastructureBootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using Domain.CategoryAgg.Repository;
+using Domain.CommentAgg.Repository;
+using Domain.OrderAgg.Repository;
+using Domain.ProductAgg.Repository;
+using Domain.RoleAgg.Repository;
+using Domain.SellerAgg.Repository;
+using Domain.SiteEntities.Repository;
+using Domain.UserAgg.Repository;
+using Infrastructure.Persistent.EfCore.CategoryAgg;
+using Infrastructure.Persistent.EfCore.CommentAgg;
+using Infrastructure.Persistent.EfCore.OrderAgg;
+using Infrastructure.Persistent.EfCore.ProductAgg;
+using Infrastructure.Persistent.EfCore.RoleAgg;
+using Infrastructure.Persistent.EfCore.SellerAgg;
+using Infrastructure.Persistent.EfCore.SiteEntities.Repositories;
+using Infrastructure.Persistent.EfCore.UserAgg;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public class InfrastructureBootstrapper
+    {
+        public void Congiure(IServiceCollection service, string connectionString)
+        {
+            service.AddTransient<ICategoryRepository, CategoryRepository>();
+            service.AddTransient<ICommentRepository, CommentRepository>();
+            service.AddTransient<IOrderRepository, OrderRepository>();
+            service.AddTransient<IProductRepository, ProductRepository>();
+            service.AddTransient<IRoleRepository, RoleRepository>();
+            service.AddTransient<ISellerRepository, SellerRepository>();
+            service.AddTransient<ISliderRepository, SliderRepository>();
+            service.AddTransient<IBannerRepository, BannerRepository>();
+            service.AddTransient<IUserRepository, UserRepository>();
+        }
+    }
+
+}
