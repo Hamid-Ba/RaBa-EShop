40239643f6ef83aee87eb772fe779963a9c4936a
hamidba <balalzadehhamid79@gmail.com>
2022-03-23 16:57:06 +0430

    Complete Get Product By Slug Query Section

---
+++ Shop/Query/ProductAgg/GetBySlug/GetProductBySlugQuery.cs
@@ -0,0 +1,7 @@
+﻿using Framework.Query;
+using Query.ProductAgg.DTOs;
+
+namespace Query.ProductAgg.GetBySlug
+{
+    public record GetProductBySlugQuery(string Slug) : IBaseQuery<ProductDto>;
+}
---
+++ Shop/Query/ProductAgg/GetBySlug/GetProductBySlugQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Query;
+using Infrastructure.Persistent.EfCore;
+using Microsoft.EntityFrameworkCore;
+using Query.ProductAgg.DTOs;
+
+namespace Query.ProductAgg.GetBySlug
+{
+    public class GetProductBySlugQueryHandler : IBaseQueryHandler<GetProductBySlugQuery, ProductDto>
+    {
+        private readonly ShopContext _context;
+
+        public GetProductBySlugQueryHandler(ShopContext context) => _context = context;
+
+        public async Task<ProductDto> Handle(GetProductBySlugQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Slug == request.Slug);
+            if (product is null) return null;
+
+            return product.MapSingle(_context);
+        }
+    }
+}
