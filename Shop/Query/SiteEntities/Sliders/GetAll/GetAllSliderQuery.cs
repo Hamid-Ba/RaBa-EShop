5d9b1a8c6a2ca5f48723fbad68be42b9339eb0c1
hamidba <balalzadehhamid79@gmail.com>
2022-03-24 10:02:56 +0430

    Complete Get All Sliders Query Section

---
+++ Shop/Query/SiteEntities/Sliders/GetAll/GetAllSliderQuery.cs
@@ -0,0 +1,7 @@
+﻿using Framework.Query;
+using Query.SiteEntities.Sliders.DTOs;
+
+namespace Query.SiteEntities.Sliders.GetAll
+{
+    public record GetAllSliderQuery : IBaseQuery<List<SliderDto>>;
+}
---
+++ Shop/Query/SiteEntities/Sliders/GetAll/GetAllSliderQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Framework.Query;
+using Infrastructure.Persistent.EfCore;
+using Microsoft.EntityFrameworkCore;
+using Query.SiteEntities.Sliders.DTOs;
+
+namespace Query.SiteEntities.Sliders.GetAll
+{
+    public class GetAllSliderQueryHandler : IBaseQueryHandler<GetAllSliderQuery, List<SliderDto>>
+    {
+        private readonly ShopContext _context;
+
+        public GetAllSliderQueryHandler(ShopContext context) => _context = context;
+
+        public async Task<List<SliderDto>> Handle(GetAllSliderQuery request, CancellationToken cancellationToken)
+        {
+            var sliders = await _context.Sliders.ToListAsync();
+
+            return sliders.Select(s => s.Map()).ToList();
+        }
+    }
+}
