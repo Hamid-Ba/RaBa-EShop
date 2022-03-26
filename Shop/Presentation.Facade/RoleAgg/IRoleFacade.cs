e6211ead85c010f722fcd0ae5889be6612cbfd14
hamidba <balalzadehhamid79@gmail.com>
2022-03-26 10:14:17 +0430

    Complete Role Facade

---
+++ Shop/Presentation.Facade/RoleAgg/IRoleFacade.cs
@@ -0,0 +1,21 @@
+﻿using Application.RoleAgg.Create;
+using Application.RoleAgg.Edit;
+using Framework.Application;
+using Query.RoleAgg.DTOs;
+
+namespace Presentation.Facade.RoleAgg
+{
+    public interface IRoleFacade
+	{
+        #region Command
+        Task<OperationResult> Edit(EditRoleCommand command);
+        Task<OperationResult> Create(CreateRoleCommand command);
+        #endregion
+
+        #region Query
+        Task<RoleDto> GetBy(long id);
+        Task<List<RoleDto>> GetAll();
+        #endregion
+
+    }
+}
---
+++ Shop/Presentation.Facade/RoleAgg/RoleFacade.cs
@@ -0,0 +1,25 @@
+﻿using Application.RoleAgg.Create;
+using Application.RoleAgg.Edit;
+using Framework.Application;
+using MediatR;
+using Query.RoleAgg.DTOs;
+using Query.RoleAgg.Get;
+using Query.RoleAgg.GetAll;
+
+namespace Presentation.Facade.RoleAgg
+{
+    public class RoleFacade : IRoleFacade
+	{
+        private readonly IMediator _mediator;
+
+        public RoleFacade(IMediator mediator) => _mediator = mediator;
+
+        public async Task<OperationResult> Create(CreateRoleCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> Edit(EditRoleCommand command) => await _mediator.Send(command);
+
+        public async Task<List<RoleDto>> GetAll() => await _mediator.Send(new GetAllRolesQuery());
+
+        public async Task<RoleDto> GetBy(long id) => await _mediator.Send(new GetRoleByIdQuery(id));
+    }
+}
