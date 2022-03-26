a2143b6c7eb8a7292e5237b187f76de517cf00e1
hamidba <balalzadehhamid79@gmail.com>
2022-03-26 10:20:55 +0430

    Complete Seller Facade

---
+++ Shop/Presentation.Facade/SellerAgg/ISellerFacade.cs
@@ -0,0 +1,26 @@
+﻿using Application.SellerAgg.AddInventory;
+using Application.SellerAgg.ChangeStatus;
+using Application.SellerAgg.Create;
+using Application.SellerAgg.Edit;
+using Application.SellerAgg.EditInventory;
+using Framework.Application;
+using Query.SellerAgg.DTOs;
+
+namespace Presentation.Facade.SellerAgg
+{
+    public interface ISellerFacade
+	{
+        #region Command
+        Task<OperationResult> Edit(EditSellerCommand command);
+        Task<OperationResult> Create(CreateSellerCommand command);
+        Task<OperationResult> AddInventory(AddInventoryCommand command);
+        Task<OperationResult> EditInventory(EditInventoryCommand command);
+        Task<OperationResult> ChangeStatus(ChangeSellerStatusCommand command);
+        #endregion
+
+        #region Query
+        Task<SellerDto> GetBy(long id);
+        Task<SellerFilterResult> GetAll(SellerFilterParam filter);
+        #endregion
+    }
+}
---
+++ Shop/Presentation.Facade/SellerAgg/SellerFacade.cs
@@ -0,0 +1,35 @@
+﻿using Application.SellerAgg.AddInventory;
+using Application.SellerAgg.ChangeStatus;
+using Application.SellerAgg.Create;
+using Application.SellerAgg.Edit;
+using Application.SellerAgg.EditInventory;
+using Framework.Application;
+using MediatR;
+using Query.SellerAgg.DTOs;
+using Query.SellerAgg.Get;
+using Query.SellerAgg.GetAll;
+
+namespace Presentation.Facade.SellerAgg
+{
+    public class SellerFacade : ISellerFacade
+	{
+        private readonly IMediator _mediator;
+
+        public SellerFacade(IMediator mediator) => _mediator = mediator;
+
+        public async Task<OperationResult> AddInventory(AddInventoryCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> ChangeStatus(ChangeSellerStatusCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> Create(CreateSellerCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> Edit(EditSellerCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> EditInventory(EditInventoryCommand command) => await _mediator.Send(command);
+
+        public async Task<SellerFilterResult> GetAll(SellerFilterParam filter) => await _mediator.Send(new GetAllSellerQuery(filter));
+
+        public async Task<SellerDto> GetBy(long id) => await _mediator.Send(new GetSellerByIdQuery(id));
+        
+    }
+}
