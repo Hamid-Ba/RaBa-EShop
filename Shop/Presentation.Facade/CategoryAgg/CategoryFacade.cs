e3c306ea8a5386fdcc653cc6aa9407e3fa929c11
hamidba <balalzadehhamid79@gmail.com>
2022-03-26 08:23:16 +0430

    Complete Category Facade

---
+++ Shop/Presentation.Facade/CategoryAgg/CategoryFacade.cs
@@ -0,0 +1,31 @@
+﻿using Application.CategoryAgg.AddChild;
+using Application.CategoryAgg.Create;
+using Application.CategoryAgg.Edit;
+using Framework.Application;
+using MediatR;
+using Query.CategoryAgg.DTOs;
+using Query.CategoryAgg.GetAll;
+using Query.CategoryAgg.GetById;
+using Query.CategoryAgg.GetChildrenById;
+
+namespace Presentation.Facade.CategoryAgg
+{
+    internal class CategoryFacade : ICategoryFacade
+    {
+        private readonly IMediator _mediator;
+
+        public CategoryFacade(IMediator mediator) => _mediator = mediator;
+
+        public async Task<OperationResult> AddChild(AddChildCategoryCommand command) =>await _mediator.Send(command);
+
+        public async Task<OperationResult> Create(CreateCategoryCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> Edit(EditCategoryCommand command) => await _mediator.Send(command);
+
+        public async Task<List<CategoryDto>> GetAll() => await _mediator.Send(new GetAllCategoryQuery());
+
+        public async Task<CategoryDto> GetCategoryById(long id) => await _mediator.Send(new GetCategoryByIdQuery(id));
+
+        public async Task<List<CategoryDto>> GetCategoryChildrenById(long parentId) => await _mediator.Send(new GetCategoryChildrenByIdQuery(parentId));
+    }
+}
---
+++ Shop/Presentation.Facade/CategoryAgg/ICategoryFacade.cs
@@ -0,0 +1,24 @@
+﻿using Application.CategoryAgg.AddChild;
+using Application.CategoryAgg.Create;
+using Application.CategoryAgg.Edit;
+using Framework.Application;
+using Query.CategoryAgg.DTOs;
+
+namespace Presentation.Facade.CategoryAgg
+{
+    public interface ICategoryFacade
+	{
+        #region Command
+        Task<OperationResult> Edit(EditCategoryCommand command);
+		Task<OperationResult> Create(CreateCategoryCommand command);
+		Task<OperationResult> AddChild(AddChildCategoryCommand command);
+        #endregion
+
+        #region Query
+        Task<List<CategoryDto>> GetAll();
+        Task<CategoryDto> GetCategoryById(long id);
+        Task<List<CategoryDto>> GetCategoryChildrenById(long parentId);
+        #endregion
+
+    }
+}
