3390a85739bc7331d5a93244a153345270b9b84c
hamidba <balalzadehhamid79@gmail.com>
2022-03-26 09:51:58 +0430

    Complete Product Facade

---
+++ Shop/Presentation.Facade/ProductAgg/IProductFacade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Application.ProductAgg.AddImage;
+using Application.ProductAgg.Create;
+using Application.ProductAgg.DeleteImage;
+using Application.ProductAgg.Edit;
+using Application.ProductAgg.EditImage;
+using Framework.Application;
+using Query.ProductAgg.DTOs;
+
+namespace Presentation.Facade.ProductAgg
+{
+	public interface IProductFacade
+	{
+        #region Command
+        Task<OperationResult> Edit(EditProductCommand command);
+        Task<OperationResult> Create(CreateProductCommand command);
+        Task<OperationResult> AddImage(AddImageProductCommand command);
+        Task<OperationResult> EditImage(EditImageProductCommand command);
+        Task<OperationResult> DeleteImage(DeleteImageProductCommand command);
+        #endregion
+
+        #region Query
+        Task<ProductDto> GeyBy(long id);
+        Task<ProductDto> GetBy(string slug);
+        Task<ProductFilterResult> GetAll(ProductFilterParam filter);
+        #endregion
+    }
+}
+
---
+++ Shop/Presentation.Facade/ProductAgg/ProductFacade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Application.ProductAgg.AddImage;
+using Application.ProductAgg.Create;
+using Application.ProductAgg.DeleteImage;
+using Application.ProductAgg.Edit;
+using Application.ProductAgg.EditImage;
+using Framework.Application;
+using MediatR;
+using Query.ProductAgg.DTOs;
+using Query.ProductAgg.GetAll;
+using Query.ProductAgg.GetById;
+using Query.ProductAgg.GetBySlug;
+
+namespace Presentation.Facade.ProductAgg
+{
+    public class ProductFacade : IProductFacade
+    {
+        private readonly IMediator _mediator;
+
+        public ProductFacade(IMediator mediator) => _mediator = mediator;
+
+        public async Task<OperationResult> AddImage(AddImageProductCommand command) =>await _mediator.Send(command);
+
+        public async Task<OperationResult> Create(CreateProductCommand command) =>await _mediator.Send(command);
+
+        public async Task<OperationResult> DeleteImage(DeleteImageProductCommand command) =>await _mediator.Send(command);
+
+        public async Task<OperationResult> Edit(EditProductCommand command) =>await _mediator.Send(command);
+
+        public async Task<OperationResult> EditImage(EditImageProductCommand command) =>await _mediator.Send(command);
+
+        public async Task<ProductFilterResult> GetAll(ProductFilterParam filter) => await _mediator.Send(new GetAllProductsQuery(filter));
+
+        public async Task<ProductDto> GetBy(string slug) => await _mediator.Send(new GetProductBySlugQuery(slug));
+
+        public async Task<ProductDto> GeyBy(long id) => await _mediator.Send(new GetProductByIdQuery(id));
+        
+    }
+}
