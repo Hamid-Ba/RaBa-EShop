f209e3007fbf00778e00e8799cc8a8b3ff3254fe
hamidba <balalzadehhamid79@gmail.com>
2022-03-26 08:34:05 +0430

    Complete Comment Facade

---
+++ Shop/Presentation.Facade/CommentAgg/CommentFacade.cs
@@ -0,0 +1,25 @@
+﻿using Application.CommentAgg.ChangeStatus;
+using Application.CommentAgg.Create;
+using Framework.Application;
+using MediatR;
+using Query.CommentAgg.DTOs;
+using Query.CommentAgg.GetAll;
+using Query.CommentAgg.GetById;
+
+namespace Presentation.Facade.CommentAgg
+{
+    public class CommentFacade : ICommentFacade
+	{
+        private readonly IMediator _mediator;
+
+        public CommentFacade(IMediator mediator) => _mediator = mediator;
+
+        public async Task<OperationResult> ChangeStatus(ChangeStatusCommentCommand command) => await _mediator.Send(command);
+
+        public async Task<OperationResult> Create(CreateCommentCommand command) => await _mediator.Send(command);
+
+        public async Task<CommentFilterResult> GetAll(CommentFilterParam filter) => await _mediator.Send(new GetAllCommentsQuery(filter));
+
+        public async Task<CommentDto> GetById(long id) => await _mediator.Send(new GetCommentByIdQuery(id));
+    }
+}
---
+++ Shop/Presentation.Facade/CommentAgg/ICommentFacade.cs
@@ -0,0 +1,21 @@
+﻿using Application.CommentAgg.ChangeStatus;
+using Application.CommentAgg.Create;
+using Framework.Application;
+using Query.CommentAgg.DTOs;
+
+namespace Presentation.Facade.CommentAgg
+{
+    public interface ICommentFacade
+	{
+        #region Command
+        Task<OperationResult> Create(CreateCommentCommand command);
+		Task<OperationResult> ChangeStatus(ChangeStatusCommentCommand command);
+        #endregion
+
+        #region Query
+        Task<CommentDto> GetById(long id);
+        Task<CommentFilterResult> GetAll(CommentFilterParam filter);
+        #endregion
+    }
+}
+
