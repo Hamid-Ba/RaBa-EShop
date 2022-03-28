69304a102438f646823defef6496c4846b6b5f91
hamidba <balalzadehhamid79@gmail.com>
2022-03-28 08:36:21 +0430

    Add Api Result Class

---
+++ Framework/Presentation/Api/ApiResult.cs
@@ -0,0 +1,29 @@
+﻿namespace Framework.Presentation.Api
+{
+    public class ApiResult
+	{
+        public bool IsSuccess { get; set; }
+        public ApiMetaDate MetaDate { get; set; }
+    }
+
+    public class ApiResult<TData> : ApiResult
+    {
+        public TData Data { get; set; }
+    }
+
+    public class ApiMetaDate
+    {
+        public string Message { get; set; }
+        public ApiStatusCode Status { get; set; }
+    }
+
+    public enum ApiStatusCode
+    {
+        Success = 1,
+        NotFound = 2,
+        BadRequest = 3,
+        LogicError = 4,
+        UnAuthorize = 5,
+        ServerError
+    }
+}
