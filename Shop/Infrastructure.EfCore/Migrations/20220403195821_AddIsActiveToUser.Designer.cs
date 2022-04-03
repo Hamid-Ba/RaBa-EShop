de85572ac91900b3b1c5288bb5dcb6d96d6ddd18
hamidba <balalzadehhamid79@gmail.com>
2022-04-04 00:31:12 +0430

    Add Migrations For User
    Adding Migrations For Adding IsActive Property To User Aggregate

--- Shop/Infrastructure.EfCore/Migrations/20220403195821_AddIsActiveToUser.Designer.cs
+++ Shop/Infrastructure.EfCore/Migrations/20220403195821_AddIsActiveToUser.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistent.EfCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20220403195821_AddIsActiveToUser")]
+    partial class AddIsActiveToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
